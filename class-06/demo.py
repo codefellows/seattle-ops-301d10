1a458ca9c50edc5a670d62d767cfae7a05a21d65
Roger Huba <rogerhuba@gmail.com>
2023-12-04 14:09:50 -0600

    add class 6 demo

---
+++ class-06/demo.py
@@ -0,0 +1,37 @@
+# Author:         Roger Huba
+# Script:         Ops 301 Ops Chall 06
+# Purpose:        Show Class Python Examples
+# Why:            New Language
+
+# Variables
+
+# This variable is assigning a string to the variable user
+# user = 'Roger'
+
+# Input
+# print(user)
+# user = input('Please Enter your Name: ')
+# print(user)
+
+# Output
+# print('There is going to be a user name printed: ' + user)
+# print(user)
+
+# Imports
+
+import os
+
+# Bash Things in Python
+
+# os.system('ls -la')
+# os.system('ifconfig')
+
+# Functions
+
+def my_function():
+  os.system('ls -la')
+  os.system('ifconfig')
+
+my_function()
+
+# Stretch: If statment
