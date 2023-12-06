a5d0a2127654b8cef9fb6636096754979abc8291
Roger Huba <rogerhuba@gmail.com>
2023-12-06 14:27:32 -0600

    update slide info in facilitator guide

---
+++ class-08/demo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# create a list
+
+#  index:     0         1        2          3             4         5
+my_list = ['apple', 'orange', 'cherry', 'watermellon', 'banana', 'kiwi']
+
+# print the entire list/array
+# print(my_list)
+
+# print the index 1 inside of the list/array called my_list
+# print(my_list[3])
+
+# Print the last item in the index?
+# print(my_list[-1])
+
+# Print Orange, Cherry, Watermellon
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+
+print(my_list[1:4])
+
+# Print the first 3 items in the list/array
+print(my_list[:3])
+
+# reverse the list/array
+
+print(my_list[::-1])
+print(my_list)
+
+my_list.append('grape')
+print(my_list)
+
